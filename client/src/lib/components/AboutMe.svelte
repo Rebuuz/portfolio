ccb070e5b6600ab94ba77274f486b56fdfd069a2
RebeccaLager <rebeccalagerr@outlook.com>
2025-09-23 23:22:16 +0200

    Added lucide
    Added lucide for icons, updated css

--- client/src/lib/components/AboutMe.svelte
+++ client/src/lib/components/AboutMe.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import myPhoto from "$lib/assets/images/rebeccalager.png";
+	import * as Accordion from "$lib/components/ui/accordion"; 
+    import { CircleArrowLeft } from "lucide-svelte";
 </script>
 
 
@@ -8,15 +10,25 @@
         <div class="aboutme-container grid grid-cols-2">
             <img alt="Rebecca Lager" src="{myPhoto}" class="mx-auto w-50 rounded-full shadow-2xl" />
             <div class="text-container">
+                <CircleArrowLeft/>
                 <h2 class="pt-10 text-left pb-5 text-2xl text-rose-300">about me</h2>
                 <p class="text-left">Here is some information about myself in general</p>
             </div>
         </div>
-        <div class="button-container grid grid-cols-2 gap-4">
-            <button>Test</button>
-            <button>Test</button>
-            <button>Test</button>
-            <button>Test</button>
-        </div>
+       <Accordion.Root type="single" class="w-xl ml-10">
+            <Accordion.Item value="item-1">
+                <Accordion.Trigger>Skills</Accordion.Trigger>
+                <Accordion.Content>
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                </Accordion.Content>
+            </Accordion.Item>
+
+            <Accordion.Item value="item-2">
+                <Accordion.Trigger>Another question?</Accordion.Trigger>
+                <Accordion.Content>
+                    Some more content here.
+                </Accordion.Content>
+            </Accordion.Item>
+        </Accordion.Root>
     </div>
 </section>
