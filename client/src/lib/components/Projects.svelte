a36f880e6e76fa075a6e0792e1b1fe4f6c6526c2
RebeccaLager <rebeccalagerr@outlook.com>
2025-10-04 19:24:15 +0200

    added project cards

---
+++ client/src/lib/components/Projects.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import * as Card from "$lib/components/ui/card"; 
+</script>
+
+<section class="projects-section" id="projects">
+    <div class="flex flex-col">
+        <h2 class="text-center text-3xl text-rose-300 pb-4">projects</h2> 
+        <p class="text-center text-[#333]">these are some of the projects i have made!</p>
+    </div>
+
+    <div class="grid grid-row grid-cols-3 gap-5 mt-20">
+        <Card.Root class="border-primary text-center">
+            <Card.Header>
+                
+                <Card.Title>Card Title</Card.Title>
+                <Card.Description>Card Description</Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <p>Card Content</p>
+            </Card.Content>
+            <Card.Footer>
+                <p>Card Footer</p>
+            </Card.Footer>
+        </Card.Root>
+        <Card.Root>
+            <Card.Header>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Description>Card Description</Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <p>Card Content</p>
+            </Card.Content>
+            <Card.Footer>
+                <p>Card Footer</p>
+            </Card.Footer>
+        </Card.Root>
+        <Card.Root>
+            <Card.Header>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Description>Card Description</Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <p>Card Content</p>
+            </Card.Content>
+            <Card.Footer>
+                <p>Card Footer</p>
+            </Card.Footer>
+        </Card.Root>
+    </div>
+</section>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
     import HeroComponent from "$lib/components/Hero.svelte";
     import AboutMe from "$lib/components/AboutMe.svelte";
+    import Projects from "$lib/components/Projects.svelte";
+	import { DivideSquareIcon } from "lucide-svelte";
 </script>
 
 <main>
     <HeroComponent></HeroComponent>
-    <AboutMe></AboutMe>
+    <div class="projects-section bg-white rounded-3xl p-20 mb-20 shadow-2xl">
+        <AboutMe></AboutMe>
+        <Projects></Projects>
+    </div>
 </main>
