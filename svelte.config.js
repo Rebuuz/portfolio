d90628bd4bd1287921daf9a70b4039c0698d8082
RebeccaLager <rebeccalagerr@outlook.com>
2025-10-21 20:56:48 +0200

    updated svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,18 +2,20 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+const dev = process.env.NODE_ENV === 'development';
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+			base: dev ? '' : '/portfolio'
 		},
 		alias: {
 			$lib: 'src/lib',
 			$routes: 'src/routes'
-		}
+		},
+		appDir: 'internal'
 	}
 };
 
