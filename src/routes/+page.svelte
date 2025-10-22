7b3fb3c73a4007055a21985cab40246b39e777b0
RebeccaLager <rebeccalagerr@outlook.com>
2025-10-22 19:34:21 +0200

    fixed projects in mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 
 <main>
     <HeroComponent/>
-    <div class="bg-white rounded-3xl md:p-20 p-5 mb-15 shadow-2xl">
+    <div class="bg-white rounded-3xl sm:p-15 md:p-10 lg:p-5 p-5 mb-10 shadow-2xl">
         <AboutMe/>
         <Projects/>
     </div>
